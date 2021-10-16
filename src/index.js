8b083cfe0a7b500357cfea94b9951baae2815da3
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-16 18:24:55 +0900

    コンポーネント化

--- src/index.js
+++ src/index.js
@@ -10,14 +10,6 @@
 
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
+import App from "./App.js";
 
 ReactDom.render(<App />, document.getElementById("root"));
